8f55f6a3b618f7ee8cfdd71af2c42bf598eb30f3
Prathviraj11 <prathviraj200011@gmail.com>
2022-05-18 11:53:16 +0530

    UPDATED

--- index.php
+++ index.php
@@ -1,22 +1,79 @@
+<!-- 
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>LOGIN</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-<body>
 
-    <form action="login.php" method="POST">
-        <h2>LOGIN</h2>
-        <?php if(isset($_GET['error'])){ ?>
-            <p class="error"> <?php echo $_GET['error']; ?> </p>
-        <?php } ?>
-        <label for="">User name</label>
-        <input type="text" name="uname" placeholder="User Name"><br>
-        <label for="pwd">Password</label>
-        <input type="password" name="password" placeholder="Password" id=""><br>
-        <button type="submit" name="" id="">LOGIN</button>
-    </form>
- 
+<head>
+    <title> Login Form in HTML5 and CSS3</title>
+    <link rel="stylesheet" a href="style.css">
+    <link rel="stylesheet" a href="css\font-awesome.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <img src="image/login.png" />
+        <form action="login.php" method="POST">
+      
+
+            <div class="form-input">
+                <input type="text" name="text" placeholder="Enter the User Name" />
+            </div>
+            <div class="form-input">
+                <input type="password" name="password" placeholder="password" />
+            </div>
+            <input type="submit" type="submit" value="LOGIN" class="btn-login" />
+        </form>
+    </div>
 </body>
+
+</html>
+
+
+ -->
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Login Form in HTML5 and CSS3</title>
+    <link rel="stylesheet" a href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="wrapper">
+        <form action="login.php" method="POST">
+            <?php if (isset($_GET['error'])) { ?>
+                <p class="error"> <?php echo $_GET['error']; ?> </p>
+            <?php } ?>
+            <div class="logo m-3">
+                <!-- <img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png" alt=""> -->
+            </div>
+            <div class="text-center mt-4 name">
+                Dairy Automation
+            </div>
+            <form class="p-3 mt-3">
+                <div class="form-field d-flex align-items-center">
+                    <span class="far fa-user"></span>
+                    <input type="text" name="userName" id="userName" placeholder="Username">
+                </div>
+                <div class="form-field d-flex align-items-center">
+                    <span class="fas fa-key"></span>
+                    <input type="password" name="password" id="pwd" placeholder="Password">
+                </div>
+                <button  type="submit" class="btn mt-3">Login</button>
+            </form>
+            <div class="text-center fs-6">
+                <a href="#">Forget password?</a> or <a href="#">Sign up</a>
+            </div>
+    </div>
+    </form>
+</body>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
 </html>
--- login.php
+++ login.php
@@ -2,48 +2,45 @@
 session_start();
 include "db_conn.php";
 
-if (isset($_POST['uname'])&& isset($_POST['password'])){
-    function validate($data){
-        $data=trim($data);
-        $data=stripslashes($data);
-        $data=htmlspecialchars($data);
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+    function validate($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
         return $data;
     }
-}
 
-$uname =validate($_POST['uname']);
-$pass =validate($_POST['password']);
 
-if(empty($uname)){
-    header("Location: index.php?error=User name is required");
-    exit();
-}
-else if(empty($pass)){
-    header("Location: index.php?error=Password is required");
-    exit();
-}
+    $uname = validate($_POST['uname']);
+    $pass = validate($_POST['password']);
 
-$sql = "SELECT * FROM userdetails where id=`$uname` AND password=`$pass`";
-
-$result =mysqli_query($conn,$sql);
-
-if(mysqli_num_rows($result)===1){
-    $row = mysqli_fetch_assoc($result);
-    if($row['id']==$uname && $row['password']===$pass){
-        echo "Logged IN!";
-        $_SESSION['id']=$row['id'];
-        $_SESSION['name']=$row['name'];
-        header("Location : home.php");
+    if (empty($uname)) {
+        header("Location: index.php?error=User name is required");
         exit();
-
-    }
-    else{
-        header("Location : index.php?error=Incorrect Username or password");
+    } else if (empty($pass)) {
+        header("Location: index.php?error=Password is required");
         exit();
-    }
+    } else {
+        $sql = "SELECT * FROM userdetails where id=`$uname` AND password=`$pass`";
 
-}
-else{
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) === 1) {
+            $row = mysqli_fetch_assoc($result);
+            if ($row['id'] == $uname && $row['password'] === $pass) {
+                echo "Logged IN!";
+                $_SESSION['id'] = $row['id'];
+                $_SESSION['name'] = $row['name'];
+                header("Location : home.php");
+                exit();
+            }
+        } else {
+            header("Location : index.php?error=Incorrect Username or password");
+            exit();
+        }
+    }
+} else {
     header("Location : index.php");
     exit();
 }
